60000b3e5baf2387c341ef188fc6f35eca385f23
cyanblot <srijanjain1207@gmail.com>
2020-11-21 00:54:13 +0530

    dynanmic with click

--- script.js
+++ script.js
@@ -48,11 +48,32 @@ window.onload = () => {
                     const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '25 25 25');
+                    placeText.setAttribute('scale', '15 15 15');
                     
-                    placeText.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
+                    placeText.addEventListener('loaded', () => { window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))});
+
+                    const clickListener = function (ev) {
+                      ev.stopPropagation();
+                      ev.preventDefault();
+
+                      const name = ev.target.getAttribute('name');
+                      const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+                      if (el && el === ev.target) {
+                          // after click, we are adding a label with the name of the place
+                          const label = document.createElement('span');
+                          const container = document.createElement('div');
+                          container.setAttribute('id', 'place-label');
+                          label.innerText = name;
+                          container.appendChild(label);
+                          document.body.appendChild(container);
+
+                          setTimeout(() => {
+                              // that will disappear after less than 2 seconds
+                              container.parentElement.removeChild(container);
+                          }, 1500);
+                       }
+                      };
 
                     scene.appendChild(placeText);
                 });
