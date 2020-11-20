1a98070d1b997b219d06cbe1b21a7859f1d1bb24
cyanblot <srijanjain1207@gmail.com>
2020-11-21 00:59:34 +0530

    title back

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onload = () => {
                     // add place name
                     const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    // placeText.setAttribute('title', place.name);
+                    placeText.setAttribute('title', place.name);
                     placeText.setAttribute('scale', '15 15 15');
                     
                     placeText.addEventListener('loaded', () => { window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))});
