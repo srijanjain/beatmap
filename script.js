be72f5bfad3d6fd21392a499ec19282802459b01
cyanblot <srijanjain1207@gmail.com>
2020-11-20 23:35:13 +0530

    scaling changes

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onload = () => {
                     const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '15 15 15');
+                    placeText.setAttribute('scale', '40 40 40');
                     
                     placeText.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
