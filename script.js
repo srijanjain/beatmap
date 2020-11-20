5345a7b86b8c4dadda5024380710a1ac7dbab047
cyanblot <srijanjain1207@gmail.com>
2020-11-21 01:00:31 +0530

    large dots

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ window.onload = () => {
                     const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '15 15 15');
+                    placeText.setAttribute('scale', '60 60 60');
                     
                     placeText.addEventListener('loaded', () => { window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))});
 
