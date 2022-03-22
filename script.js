5c98b8926c8475df88850680b4c6d05d032cf301
Srijan Jain <srijanjain1207@gmail.com>
2022-03-22 19:18:32 +0530

    updated script

--- script.js
+++ script.js
@@ -1,93 +1,90 @@
 const loadPlaces = function (coords) {
-  const method = "api"; // COMMENT LINE IF STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
+  // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
+  const method = 'api';
 
   const PLACES = [
-    {
-      name: "Your place name",
-      location: {
-        lat: 0, // latitude if using static data
-        lng: 0, // longitude if using static data
+      {
+          name: "Your place name",
+          location: {
+              lat: 0, // add here latitude if using static data
+              lng: 0, // add here longitude if using static data
+          }
       },
-    },
   ];
 
-  if (method === "api") {
-    return loadPlaceFromAPIs(coords);
+  if (method === 'api') {
+      return loadPlaceFromAPIs(coords);
   }
 
-  return PLACES;
+  return Promise.resolve(PLACES);
 };
 
 // getting places from REST APIs
 function loadPlaceFromAPIs(position) {
   const params = {
-    radius: 500, // search places not farther than this value (in meters)
-    clientId: "D3AOLPGR40ZWOHZ54XKXJGORGFHZY31U5FRSXRUHHMKRXHKD",
-    clientSecret: "2UM3SJVRFCJK4KQ43H1VADXYBAVUB0AVIVRXGMGTQOWJMJKX",
-    version: "20300101", // foursquare versioning
+      radius: 300,    // search places not farther than this value (in meters)
+      clientId: 'D3AOLPGR40ZWOHZ54XKXJGORGFHZY31U5FRSXRUHHMKRXHKD',
+      clientSecret: '2UM3SJVRFCJK4KQ43H1VADXYBAVUB0AVIVRXGMGTQOWJMJKX',
+      version: '20300101',    // foursquare versioning, required but unuseful for this demo
   };
 
-  // CORS Proxy
-  const corsProxy = "https://cors-anywhere.herokuapp.com/";
+  // CORS Proxy to avoid CORS problems
+  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
-  // Foursquare API for locations of interest
+  // Foursquare API
   const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=15
-        &v=${params.version}`;
+      &ll=${position.latitude},${position.longitude}
+      &radius=${params.radius}
+      &client_id=${params.clientId}
+      &client_secret=${params.clientSecret}
+      &limit=15
+      &v=${params.version}`;
   return fetch(endpoint)
-    .then((res) => {
-      return res.json().then((resp) => {
-        return resp.response.venues;
-      });
-    })
-    .catch((err) => {
-      console.error("Error with places API", err);
-    });
-}
+      .then((res) => {
+          return res.json()
+              .then((resp) => {
+                  return resp.response.venues;
+              })
+      })
+      .catch((err) => {
+          console.error('Error with places API', err);
+      })
+};
+
 
 window.onload = () => {
-  const scene = document.querySelector("a-scene");
+  const scene = document.querySelector('a-scene');
 
-  // Getting current user location
-  return navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // Using current location to load from remote APIs some places nearby
-      loadPlaces(position.coords).then((places) => {
-        places.forEach((place) => {
-          const latitude = place.location.lat;
-          const longitude = place.location.lng;
-          //77.23,88.55 as a string (key)=> string(value) 
-          console.log(longitude);
-          console.log(latitude);
+  // first get current user location
+  return navigator.geolocation.getCurrentPosition(function (position) {
 
-          // add place name
-          const text = document.createElement("a-link");
-          text.setAttribute(
-            "gps-entity-place",
-            `latitude: ${latitude}; longitude: ${longitude};`
-          );
-          text.setAttribute("title", place.name);
-          text.setAttribute(
-            "href",
-            "https://srijanja.in/beatmap/beatbuilder/"
-          );
-          text.setAttribute("scale", "25 25 25");
-          text.addEventListener("loaded", () => {
-            window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
-          });
-          scene.appendChild(text);
-        });
-      });
-    },
-    (err) => console.error("Error in retrieving position", err),
-    {
-      enableHighAccuracy: true,
-      maximumAge: 0,
-      timeout: 27000,
-    }
+      // than use it to load from remote APIs some places nearby
+      loadPlaces(position.coords)
+          .then((places) => {
+              places.forEach((place) => {
+                  const latitude = place.location.lat;
+                  const longitude = place.location.lng;
+
+                  // add place name
+                  const text = document.createElement('a-link');
+                  text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                  text.setAttribute('title', place.name);
+                  text.setAttribute('href', 'http://www.example.com/');
+                  text.setAttribute('scale', '20 20 20');
+
+                  text.addEventListener('loaded', () => {
+                      window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                  });
+
+                  scene.appendChild(text);
+              });
+          })
+  },
+      (err) => console.error('Error in retrieving position', err),
+      {
+          enableHighAccuracy: true,
+          maximumAge: 0,
+          timeout: 27000,
+      }
   );
 };
